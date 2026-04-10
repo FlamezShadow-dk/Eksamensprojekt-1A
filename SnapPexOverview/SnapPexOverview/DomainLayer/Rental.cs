aeab58e8c4230670ec6b57b04a33331787775ef4
Tia <VTia@proton.me>
2026-04-10 15:18:02 +0200

    Domain classes & enum v1, changes likely necessary

--- SnapPexOverview/SnapPexOverview/DomainLayer/Rental.cs
+++ SnapPexOverview/SnapPexOverview/DomainLayer/Rental.cs
@@ -6,5 +6,16 @@ namespace SnapPexOverview.DomainLayer
 {
     public class Rental
     {
+        public string CompanyName { get; set; }
+
+        public static void StartDate()
+        {
+            DateOnly startDate = default; // DateOnly might not work; supposedly unavailable for .NET framework
+        }
+
+        public static void EndDate()
+        {
+            DateOnly? endDate = null;
+        }
     }
 }
