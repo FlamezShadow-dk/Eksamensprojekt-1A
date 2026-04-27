9f3a5a4bd48bf3cef202d39fd9c8cd0394e0b8ba
Christian Jensen <kanon8578@gmail.com>
2026-04-27 11:03:50 +0200

    Added a GetById and an Update method

--- SnapPexOverview/SnapPexOverview/PersistenceLayer/MachineRepository.cs
+++ SnapPexOverview/SnapPexOverview/PersistenceLayer/MachineRepository.cs
@@ -1,10 +1,54 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using SnapPexOverview.DomainLayer;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
+
 
 namespace SnapPexOverview.PersistenceLayer
 {
-    public class MachineRepository
+    public class MachineRepository : Repository
     {
+        private List<Machine> machines = new List<Machine>();
+
+
+
+
+        public Machine GetByID(int machineNr)
+        {
+            Machine result = null;
+
+            using (SqlConnection con = CreateConnection())
+            using (SqlCommand cmd = new SqlCommand("SELECT * FROM MACHINE WHERE MachineNr = @MachineNr", con))
+            {
+                cmd.Parameters.AddWithValue("@machineNr", machineNr);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        Machine mac = new Machine(machineNr);
+
+                        mac.MachineNr = (int)reader["MachineNr"];
+                        mac.Status = (MachineStatus)(int)reader["MachineStatus"];
+                        result = mac;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public void Update(Machine machine)
+        {
+            using (SqlConnection con = CreateConnection())
+            using (SqlCommand cmd = new SqlCommand("UPDATE MACHINE SET " +
+                "MachineStatus = @MachineStatus " +
+                "WHERE MachineNr = @machineNr;", con))
+            {
+                cmd.Parameters.AddWithValue("@MachineStatus", machine.Status);
+                cmd.Parameters.AddWithValue("@machineNr", machine.MachineNr);
+                cmd.ExecuteNonQuery();
+            }
+        }
     }
 }
