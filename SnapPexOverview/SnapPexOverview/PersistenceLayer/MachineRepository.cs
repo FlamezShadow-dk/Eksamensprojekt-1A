627103c83bc336b9c9cb604ac6c4eb4233acc185
Alabaster550 <vinbjo152@gmail.com>
2026-04-28 08:09:04 +0200

    finished Add, GetAll and constructor of machine repo

--- SnapPexOverview/SnapPexOverview/PersistenceLayer/MachineRepository.cs
+++ SnapPexOverview/SnapPexOverview/PersistenceLayer/MachineRepository.cs
@@ -12,8 +12,48 @@ namespace SnapPexOverview.PersistenceLayer
     {
         private List<Machine> machines = new List<Machine>();
 
+        public MachineRepository()
+        {
+            GetAll();
+        }
 
+        // returns id of machine
+        public int Add(Machine machine)
+        {
+            int result = -1;
 
+            using (SqlConnection con = CreateConnection())
+            using (SqlCommand cmd = new SqlCommand("spInsertMachine", con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@MachineStatus", machine.Status);
+                result = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+            return result;
+        }
+
+        public List<Machine> GetAll()
+        {
+            machines.Clear();
+
+            using(SqlConnection con = CreateConnection())
+            using(SqlCommand cmd = new SqlCommand("SELECT * FROM MACHINE", con))
+            {
+                using(SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int nr = Convert.ToInt32(reader["MachineNr"]);
+                        Machine machine = new Machine(nr);
+                        machine.Status = (MachineStatus)(int)reader["MachineStatus"];
+
+                        machines.Add(machine);
+                    }
+                }
+            }
+
+            return machines;
+        }
 
         public Machine GetByID(int machineNr)
         {
