fa3fdf253f956f702f9335d958c13b837be769fe
Alabaster550 <vinbjo152@gmail.com>
2026-05-06 09:15:37 +0200

    add machine databinding and business logic

--- Statements/altertable.sql
+++ Statements/altertable.sql
@@ -15,3 +15,12 @@ BEGIN
 	INSERT INTO COMPONENT(ComponentName, AmountPerMachine, AmountInStock, ImagePath)
 	VALUES(@ComponentName, @AmountPerMachine, @AmountInStock, @ImagePath);
 END
+
+ALTER PROCEDURE spInsertMachine
+	@MachineStatus INT
+AS
+BEGIN
+	INSERT INTO MACHINE(MachineStatus)
+	VALUES(@MachineStatus)
+	SELECT SCOPE_IDENTITY()
+END
